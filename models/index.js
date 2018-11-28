c022fe157d8caaa7b1d4cb9214f496e805c36f86
rafaelvalentine <ozichukwuezike@hotmail.com>
2018-11-28 01:31:27 +0100

    added mlab hook

--- models/index.js
+++ models/index.js
@@ -3,7 +3,7 @@ mongoose.set('debug', true)
 
 let dev_db_url = 'mongodb://ownit-africa_db:ownit_africa4life@ds135413.mlab.com:35413/ownit-africa_test-db'
 let mongoDB = process.env.MONGO_URI || dev_db_url
-mongoose.connect('mongodb://localhost/ownit-api' || mongoDB, {
+mongoose.connect(mongoDB || 'mongodb://localhost/ownit-api', {
     useCreateIndex: true,
     useNewUrlParser: true
 })
