d6d39bebdf2357bdd3cec7ad832af6023a3dce89
rafaelvalentine <ozichukwuezike@hotmail.com>
2018-12-24 06:13:19 +0100

    modified login

--- index.js
+++ index.js
@@ -65,9 +65,6 @@ app.get('/', (req, res) => {
     res.sendFile('index.html')
 })
 
-app.get('/register', (req, res) => {
-    res.sendFile(path.join(__dirname + '/views/sign-up.html'))
-})
 
 
 app.use('/api/ownit', ownitRoute)
