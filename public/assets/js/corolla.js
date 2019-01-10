8149d0eadc5bf82532ae6677c09efbd6c4042124
rafaelvalentine <ozichukwuezike@hotmail.com>
2019-01-10 08:12:59 +0100

    minor update to corolla page

--- public/assets/js/corolla.js
+++ public/assets/js/corolla.js
@@ -44,12 +44,14 @@ function getCar(car) {
         '</div>' +
         '</div>')
     let newCar2 = $('<div>' +
-        `<div style="${style}" class="image-imitation">` +
+        `<div style="${style}" class="image-imitation image-imitation-two">` +
         '</div>' +
+        '<div class="image-imitation-two-text">' +
         `<p class="card-text car-text">${car.make} ${car.model} ${car.year}</p>` +
         `<p class="card-text car-text-details"> <span>Weekly payment:</span> N ${car.weeklypayment} </p>` +
         `<p class="card-text car-text-details"> <span>Down payment:</span> N ${car.downpayment}</p>` +
         `<p class="card-text car-text"><span>Number of weeks:</span> 104 </p>` +
+        '</div>' +
         '</div>')
     $('.product-images').slick('slickAdd', newCar)
 
@@ -57,66 +59,85 @@ function getCar(car) {
     $('.product-images-shower').slick('slickAdd', newCarSmaller)
     $('.select-product-images').slick('slickAdd', newCar2)
 }
-
+/**
+ * Cars to select on the page
+ */
 const carInfo = [{
-        'make': 'toyota',
-        'model': 'corolla',
-        'year': '2005',
-        'downpayment': '100,000',
-        'carImage1': '',
-        'carImage2': '',
-        'carImage3': '',
-        'weeklypayment': '25,000'
-    },
-    {
-        'make': 'toyota',
-        'model': 'corolla',
-        'year': '2006',
-        'downpayment': '100,000',
-        'carImage1': '',
-        'carImage2': '',
-        'carImage3': '',
-        'weeklypayment': '25,000'
-    },
-    {
-        'make': 'toyota',
-        'model': 'corolla',
-        'year': '2007',
-        'downpayment': '100,000',
-        'carImage1': '',
-        'carImage2': '',
-        'carImage3': '',
-        'weeklypayment': '25,000'
-    },
-    {
-        'make': 'toyota',
-        'model': 'corolla',
-        'year': '2008',
-        'downpayment': '100,000',
-        'carImage1': '',
-        'carImage2': '',
-        'carImage3': '',
-        'weeklypayment': '25,000'
-    },
-    {
-        'make': 'toyota',
-        'model': 'corolla',
-        'year': '2009',
-        'downpayment': '100,000',
-        'carImage1': '',
-        'carImage2': '',
-        'carImage3': '',
-        'weeklypayment': '25,000'
-    },
-    {
-        'make': 'toyota',
-        'model': 'corolla',
-        'year': '2010',
-        'downpayment': '100,000',
-        'carImage1': '',
-        'carImage2': '',
-        'carImage3': '',
-        'weeklypayment': '25,000'
+            'make': 'toyota',
+            'model': 'corolla',
+            'year': '2005',
+            'downpayment': '100,000',
+            'carImage1': '',
+            'carImage2': '',
+            'carImage3': '',
+            'weeklypayment': '25,000'
+        },
+        {
+            'make': 'toyota',
+            'model': 'corolla',
+            'year': '2006',
+            'downpayment': '100,000',
+            'carImage1': '',
+            'carImage2': '',
+            'carImage3': '',
+            'weeklypayment': '25,000'
+        },
+        {
+            'make': 'toyota',
+            'model': 'corolla',
+            'year': '2007',
+            'downpayment': '100,000',
+            'carImage1': '',
+            'carImage2': '',
+            'carImage3': '',
+            'weeklypayment': '25,000'
+        },
+        {
+            'make': 'toyota',
+            'model': 'corolla',
+            'year': '2008',
+            'downpayment': '100,000',
+            'carImage1': '',
+            'carImage2': '',
+            'carImage3': '',
+            'weeklypayment': '25,000'
+        },
+        {
+            'make': 'toyota',
+            'model': 'corolla',
+            'year': '2009',
+            'downpayment': '100,000',
+            'carImage1': '',
+            'carImage2': '',
+            'carImage3': '',
+            'weeklypayment': '25,000'
+        },
+        {
+            'make': 'toyota',
+            'model': 'corolla',
+            'year': '2010',
+            'downpayment': '100,000',
+            'carImage1': '',
+            'carImage2': '',
+            'carImage3': '',
+            'weeklypayment': '25,000'
+        }
+    ]
+    /**
+     * toggle middle display to block
+     */
+$('#full_details-bottom').click(() => {
+    if ($('#show-icon').hasClass('fa-plus')) {
+        $('#show-icon').toggleClass('fa-plus')
+        $('#show-icon').addClass('fa-minus')
+    } else {
+        $('#show-icon').removeClass('fa-minus')
+        $('#show-icon').addClass('fa-plus')
     }
-]
+    $('#full_details-middle ').toggleClass('display-none')
+})
+
+/**
+ * for form validation
+ */
 const form = $('#ownItForm-main')
